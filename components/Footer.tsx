183faf305c29043b5fdd87ed637b049e77b7516f
Sami <mdsamialbadhon@gmail.com>
2025-11-14 12:44:27 +0600

    social media link updated and removed linkedin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
           </h2>
           <div className="flex gap-2 justify-center items-center">
             {/* facebook */}
-            <Link href="https://www.facebook.com/samidotdev.official">
+            <Link href="https://www.facebook.com/codenix.agency">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
@@ -41,7 +41,7 @@ const Footer = () => {
             </Link>
 
             {/* X */}
-            <Link href="https://x.com/sami_dotdev">
+            <Link href="https://x.com/codenix_agency">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
@@ -67,27 +67,8 @@ const Footer = () => {
               </svg>
             </Link>
 
-            {/* linkedin */}
-            <Link href="https://www.linkedin.com/in/md-sami-al-badhon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                width="50"
-                height="50"
-                viewBox="0 0 48 48"
-                className="group">
-                <path
-                  d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"
-                  className="fill-white/80 lg:group-hover:fill-[#0288D1] transition duration-300"></path>
-                <path
-                  d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"
-                  className="fill-black lg:group-hover:fill-white transition duration-300"></path>
-              </svg>
-            </Link>
-
             {/* Instagram */}
-            <Link href="https://www.instagram.com/samidotdev">
+            <Link href="https://www.instagram.com/codenix.agency">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
